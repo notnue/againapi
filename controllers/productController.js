3face36a69d4a103e43122ec9d3fef5ae27e48b8
Neelesh Patel <84118988+notnue@users.noreply.github.com>
2023-01-10 16:51:47 +0530

    second commit

--- controllers/productController.js
+++ controllers/productController.js
@@ -6,22 +6,73 @@ const product_all = async (req,res) => {
     try{
         const products=await Product.find();
         console.log(products, "fghbjnjk")
-        res.send(products);
+        res.json(products);
         console.log("sadshhyfguhfd")
 
     }catch(error){
-        res.send({message: error})
+        res.json({message: error})
     }
 
 }
 //single product
-const product_details = async(req,res) => {}
+const product_details = async(req,res) => {
+    try{
+        const products=await Product.findById(req.params.ProductId);
+        //console.log(mongoose.Types.ObjectId.isValid(req.params.locationid))
+        res.json(products);
+        console.log("sadsdtfyghujikolphhyfguhfd")
+
+    }catch(error){
+        res.json({message: error})
+    }
+}
 //add new product
-const product_create = async(req,res) => {}
+const product_create = async(req,res) => {
+    const product = new Product({
+        name: req.body.name,
+        detail: req.body.detail,
+        price: req.body.price,
+        hero: req.body.hero,
+        image: req.body.image
+    });
+        try {
+            const savedProduct = await product.save();
+            res.send(savedProduct);
+        }catch (error){
+            res.status(400).send(error);
+        }
+}
 //update product
-const product_update = async(req,res) => {}
+const product_update = async(req,res) => {
+    try{
+    const product ={
+            name: req.body.name,
+            detail: req.body.detail,
+            price: req.body.price,
+            hero: req.body.hero,
+            image: req.body.image
+        };
+
+        const updatedProduct = await Product.findByIdAndUpdate({
+            _id: req.params.productId
+        }, product);
+        res.json(updatedProduct);
+        }catch (error) {
+            res.json({message: error})
+        }
+    }
+
+
 //delete product
-const product_delete = async(req,res) => {}
+const product_delete = async(req,res) => {
+    try{    
+        const removeProduct = await Product.findByIdAndDelete(
+        req.params.productId);
+            res.json(removeProduct);
+            }catch (error) {
+                res.json({message: error})
+            }
+}
 
 
 
