b941ba3075fcb80566460709d3709ee4c8f0c82f
Neelesh Patel <84118988+notnue@users.noreply.github.com>
2023-01-10 16:56:40 +0530

    second commit

--- model/Product.js
+++ model/Product.js
@@ -1,10 +1,14 @@
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
+    //_id: mongoose.ObjectId,
     name: String,
     detail: String,
-    price: Number,
+    price: String,
+    hero: String,
     image: String
 });
+
+
 
 module.exports = mongoose.model("Product", productSchema)
