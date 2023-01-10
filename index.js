cf6cc2ba683f25005218a4bd498de0960d93dc22
Neelesh Patel <84118988+notnue@users.noreply.github.com>
2023-01-10 17:04:27 +0530

    second commit

--- index.js
+++ index.js
@@ -3,15 +3,22 @@ const express = require("express");
 const app = express(); //url vgera jo create krna ho uske liye
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const cors = require("cors")
 
 dotenv.config();
 
 //db kae connection k liye
-mongoose.connect(
+/*mongoose.connect(
     process.env.DB_CONNECT, 
-    { userUnifiedTopology: true, useNewUrlParser: true},
+    {useNewUrlParser: true},
     () => console.log("connected yo db")
-); 
+); */
+
+mongoose.connect(process.env.DB_CONNECT, {
+    useNewUrlParser:true
+},)
+.then(()=> console.log("connected")).catch(e=>console.log(e))
+
 mongoose.set('strictQuery', true);
 mongoose.set('debug', true)
 
@@ -20,8 +27,13 @@ const productRoutes = require("./routes/product", ()=>{
     console.log("hello")
 });
 
+//middlewares
+app.use(express.json())
+app.use(cors())
+
 //route middlewares
 app.use("/api/products", productRoutes)
+
 app.listen(4000, ()=> {
     console.log("the server is up and running on 4000");
 })
